8b220684d63496a7ae684c0b5f142da90b1f7baf
Paul Kasidit <paulkasidit@gmail.com>
2022-06-28 20:43:07 -0700

    managed to get first test to pass
    Co-authored-by: Niko Rivas <rivasniko@gmail.com>
    Co-authored-by: Rafael Petrachini <raffp@me.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,48 @@
+//Test: "For words beginning with a vowel, add "way" to the end. "
+//Code: pigLatin("away");
+//Expected Output: "awayway"
+
+//business user logic
+
+function getFirstLetters(str){
+  const firstLetters = str.split("").map(word => word[0]).join(''); 
+
+  return firstLetters
+}
+
 function pigLatin(event){ 
+  event.preventDefault();
 
   const wordArray = document.getElementById("wordArray").value;
+  const vowelArray = ["a", "e", "i", "o", "u"];
+  const firstLettersOfwordArray = getFirstLetters(wordArray);
 
-  console.log(wordArray)
+  console.log(firstLettersOfwordArray); 
+
+  if (wordArray.length > 1 && vowelArray.indexOf(wordArray) !== -1 ){
+    console.log("pass");
+
+  } else {
+    console.log("fail")
+    return false;
+  }
+
+  console.log("input: " + wordArray);
 
 }
 
+//user interface logic
+
 window.addEventListener("load", function(){
-  event.preventDefault();
+  //event.preventDefault();
 
   const form  = document.getElementById("pigLatinForm");
   form.addEventListener("submit", pigLatin);
 
 });
+
+
+
+
+
+ 
